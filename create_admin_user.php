444a09500d73ebaab426b9031f5d16e6dc751b18
mohitkumartyagi <mohitime2007@gmail.com>
2019-01-22 00:18:07 +0530

    admin user

---
+++ create_admin_user.php
@@ -0,0 +1,26 @@
+<?php
+    define( 'USERNAME', 'mohit' );
+    define( 'PASSWORD', 'mohit123' );
+    define( 'FIRSTNAME', 'Mohit' );
+    define( 'LASTNAME', 'Tyagi' );
+    define( 'EMAIL', 'mohitime2007@gmail.com' );
+    include_once( 'app/Mage.php' );
+    Mage::app( 'admin' );
+    try {
+        $adminUserModel = Mage::getModel( 'admin/user' );
+        $adminUserModel->setUsername( USERNAME )
+            ->setFirstname( FIRSTNAME )
+            ->setLastname( LASTNAME )
+            ->setEmail( EMAIL )
+            ->setNewPassword( PASSWORD )
+            ->setPasswordConfirmation( PASSWORD )
+            ->setIsActive( true )
+            ->save();
+        $adminUserModel->setRoleIds( array( 1 ) )
+            ->setRoleUserId( $adminUserModel->getUserId() )
+            ->saveRelations();
+        echo 'Admin User created. ';// . EOL;
+    } catch( Exception $e ) {
+        echo $e->getMessage();
+    }
+?>
